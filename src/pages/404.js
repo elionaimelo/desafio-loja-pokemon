d6c12659880fecd2c79d4326d06b5672dcba444d
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2021-08-16 09:45:31 -0300

    404 vercel

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,14 @@
-export default function Custom404() {
-  return null;
-}
+import Link from 'next/link';
 
-export function getStaticProps() {
-  return {
-    redirect: {
-      permanent: true,
-      destination: '/',
-    },
-    fallback: false,
-  };
+export default function Custom404() {
+  return (
+    <div className="flex h-screen flex-col justify-center items-center">
+      <h1>404 - Página não encontrada</h1>
+      <Link href="/">
+        <a className="bg-blue-600 px-4 py-2 text-white rounded-sm mt-4">
+          Voltar para home
+        </a>
+      </Link>
+    </div>
+  );
 }
